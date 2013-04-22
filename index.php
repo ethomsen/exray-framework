7e2fcc4acd900ede3eb9a85a69f0ac9d3129830d
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2013-04-22 14:31:25 +0700

    Remove unnecessary HTML, Validate $options value

--- index.php
+++ index.php
@@ -5,21 +5,22 @@
 			
 			<div class="row">
 			
-				<div class="span6 article-container-adaptive" id="primary">
+				<div class="span6 article-container-adaptive" id="main">
 					<?php 
 
 					$options = get_option('exray_theme_display_options');
-					if($options['show_slider']): ?>
+
+					if(isset($options['show_slider']) && $options['show_slider']): ?>
 					<!-- Experimental -->
 					<div class="slider">
 						<div class="url-container format-link"><blockquote><h1>This is SLider content Example </h1></blockquote></div>
 					</div>
-					<!-- End Experimental -->
+					End Experimental
 					
 					<?php endif; ?>
 					
 					<?php 
-					if(get_option('show_quiz')): ?>
+					if( get_option('show_quiz')): ?>
 					<!-- Experimental -->
 					<div class="quiz">
 						<div class="url-container format-link"><blockquote><h1>This is QUIZZZ! </h1></blockquote></div>
@@ -30,7 +31,7 @@
 
 					<?php 
 					$options = get_option('exray_manage_coupon_options');
-					if($options['show_coupon']): ?>
+					if(isset($options['show_coupon']) && $options['show_coupon']): ?>
 					<!-- Experimental -->
 					<div class="quiz">
 						<div class="url-container format-link"><blockquote><h1>This is COUPON! </h1></blockquote></div>
@@ -48,6 +49,10 @@
 						}
 
 						$social_options = get_option('exray_theme_social_options');
+
+						if(isset($options['checkbox_element']) && $social_options['exray_theme_twitter'] ){
+							echo '<a href="'.$social_options['exray_theme_twitter'].'">@fujianto</a>';
+						}
 						echo '<a href="'.$social_options['exray_theme_twitter'].'">@fujianto</a>';
 
 					?>	
@@ -57,23 +62,23 @@
 						echo sanitize_text_field( $options['input_element'] );
 						echo sanitize_text_field( $options['textarea_element'] );
 
-						if($options['checkbox_element'] == '1'){
+						if(isset($options['checkbox_element']) && $options['checkbox_element'] == '1'){
 							echo '<h2>Checkbox Checked</h2>';
 						}else{
 							echo '<h3>NOT CHecked</h3>';
 						}
 
-						if($options['radio_element'] == '1'){
+						if(isset($options['radio_element']) && $options['radio_element'] == '1'){
 							echo '<h2>MALE</h2>';
-						}else if($options['radio_element'] == '2'){
+						}else if(isset($options['radio_element']) && $options['radio_element'] == '2'){
 							echo '<h3>FEMALE</h3>';
 						}
 
-						if($options['select_element'] == 'never'){
+						if(isset($options['select_element']) &&  $options['select_element'] == 'never'){
 							echo '<h2>never</h2>';
-						} else if($options['select_element'] == 'sometimes'){
+						} else if(isset($options['select_element']) &&  $options['select_element'] == 'sometimes'){
 							echo '<h2>sometimes</h2>';
-						} else if($options['select_element'] == 'always'){
+						} else if(isset($options['select_element']) &&  $options['select_element'] == 'always'){
 							echo '<h2>always</h2>';
 						} else{
 							echo '<h2>Default</h2>';
@@ -97,35 +102,24 @@
 
 							<p class="article-nav-prev"><?php next_posts_link(__('&larr; Older Post', 'exray-framework')); ?></p>
 	    					<p class="article-nav-next"><?php previous_posts_link(__('Newer Post &rarr; ', 'exray-framework')); ?></p>
-
-							<!-- <ul>
-		    					<li><a class="selected" href="#">1</a></li>
-		    					<li><a href="#">2</a></li>
-		    					<li><a href="#">3</a></li>
-		    					<li><a href="#">4</a></li>
-		    					<li><a href="#">5</a></li>
-		    					<li><a href="#">6</a></li>
-	    						<li><a href="#">Next Page >></a></li> 
 	
-	    					</ul> -->
-							
 						</nav>	
 						<!-- End nav-below -->	
 					</div> 
 					<!-- end content -->
 				</div> 
-				<!-- end span6 primary -->	
+				<!-- end span6 main -->	
 					
-				<div id="secondary" class="widget-area span3 main-sidebar" role="complementary">
+				<div id="primary" class="widget-area span3 main-sidebar" role="complementary">
 
 					<?php get_sidebar('sidebar'); ?>
 
 				</div>  
 				<!-- end span3 secondary left-sidebar -->	
 
-				<div id="tertiary" class="widget-area span3 main-sidebar" role="complementary">						
+				<div id="secondary" class="widget-area span3 main-sidebar" role="complementary">						
 
-					<?php get_sidebar('tertiary'); ?>
+					<?php get_sidebar('secondary'); ?>
 							
 				</div> 
 				<!-- end span3 tertiary right-sidebar -->
