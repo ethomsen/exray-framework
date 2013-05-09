dce4e64c7546d1053aaa986ea7f82b8cf6ad9e3c
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2013-05-09 17:34:49 +0700

    Delete unused file

--- sidebar-header.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('header-widget') ) : ?>
-	
-	<aside id="header-widget" class="right-header-widget fr top-widget" role="complementary">					
-		<?php get_search_form(); ?>
-	</aside>	
-	<!-- End header-widget -->
-
-<?php endif ?>
