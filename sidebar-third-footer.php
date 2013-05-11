23da2dd0a01c4d593a42cec85f5b7953fde48cef
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2013-05-11 16:14:40 +0700

    Modify default widget title to h4

--- sidebar-third-footer.php
+++ sidebar-third-footer.php
@@ -3,7 +3,7 @@
 
 	<div class="span3">
 		<aside class="footer-widget" role="complementary">
-			<?php the_widget('WP_Widget_Recent_Posts'); ?>  
+			<?php the_widget('WP_Widget_Recent_Posts', '', array('before_title' => '<h4>', 'after_title' => '</h4>') ); ?>  
 		</aside>
 	</div>
 
