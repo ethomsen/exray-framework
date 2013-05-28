ee685cb5e34f1f1499d9c135e5677525cf63138e
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2013-05-28 13:49:20 +0700

    Add WordPress seo breadcrumb support

--- 404.php
+++ 404.php
@@ -12,32 +12,34 @@
 			
 				<div class="span12 article-container-adaptive">
 					
+					<?php Exray::load_breadcrumb(); ?>
+
 					<div class="content" role="main">
 									
-							<article class="post clearfix" id="post-1" role="article">
-								
-								<header>
-									
-									<h1 class="entry-title" id="search-title"><?php _e('404 Error - Page not Found', 'exray-framework'); ?></h1>
-									
-								</header>
+						<article class="post clearfix" id="post-1" role="article">
 							
-								<div class="entry-content">	
+							<header>
+								
+								<h1 class="entry-title" id="search-title"><?php _e('404 Error - Page not Found', 'exray-framework'); ?></h1>
+								
+							</header>
+						
+							<div class="entry-content">	
 
-									<p><?php _e('Oops! It seems what you are looking are missing, You can search more to find other relevant contents.', 'exray-framework'); ?></p>
+								<p><?php _e('Oops! It seems what you are looking are missing, You can search more to find other relevant contents.', 'exray-framework'); ?></p>
 
-									<div class="search-form-404">
-										<?php get_search_form(); ?>
-										
-										<?php the_widget('WP_Widget_Categories', 'dropdown=1&count=1'); ?>
-										<br>
-										<?php the_widget( 'WP_Widget_Tag_Cloud'); ?>  
-									</div>
+								<div class="search-form-404">
+									<?php get_search_form(); ?>
 									
+									<?php the_widget('WP_Widget_Categories', 'dropdown=1&count=1'); ?>
+									<br>
+									<?php the_widget( 'WP_Widget_Tag_Cloud'); ?>  
 								</div>
-											
-							</article> 	
-							<!-- End article -->	
+								
+							</div>
+										
+						</article> 	
+						<!-- End article -->	
 					</div> 
 					<!-- end content -->
 				</div> 
