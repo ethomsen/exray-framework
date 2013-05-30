0ddd3ac906ca4f28d22f9b5817c8db81dbd6ec82
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2013-05-30 22:43:52 +0700

    Add new class on <hgroup>

--- header.php
+++ header.php
@@ -93,7 +93,7 @@
 							<?php if(is_home()) : ?>
 
 								<!-- Display text logo and tagline	 -->
-								<hgroup>
+								<hgroup class="text-logo">
 									<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 									<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 								</hgroup>
@@ -101,7 +101,7 @@
 							<?php else: ?>
 								
 								<!-- Display text logo and tagline	 -->
-								<hgroup>
+								<hgroup class="text-logo">
 									<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 									<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 								</hgroup>
