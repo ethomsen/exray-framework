b20c572ce968730409d54964b61ec9b930849a85
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2013-06-16 05:32:29 +0700

    Minor Fix

--- 404.php
+++ 404.php
@@ -1,52 +1,52 @@
 <?php
 /*
-    Template Name: Full Width Page
-*/
+  Template Name: Full Width Page
+ */
 ?>
 <?php get_header(); ?>
 
-	<!-- Main Content -->
-		<div class="container" id="main-container">
-			
-			<div class="row">
-			
-				<div class="span12 article-container-adaptive">
-					
-					<?php Exray::load_breadcrumb(); ?>
+<!-- Main Content -->
+<div class="container" id="main-container">
 
-					<div class="content" role="main">
-									
-						<article class="post clearfix" id="post-1" role="article">
-							
-							<header>
-								
-								<h1 class="entry-title" id="search-title"><?php _e('404 Error - Page not Found', 'exray-framework'); ?></h1>
-								
-							</header>
-						
-							<div class="entry-content">	
+    <div class="row">
 
-								<p><?php _e('Oops! It seems what you are looking are missing, You can search more to find other relevant contents.', 'exray-framework'); ?></p>
+        <div class="span12 article-container-adaptive">
 
-								<div class="search-form-404">
-									<?php get_search_form(); ?>
-									
-									<?php the_widget('WP_Widget_Categories', 'dropdown=1&count=1'); ?>
-									<br>
-									<?php the_widget( 'WP_Widget_Tag_Cloud'); ?>  
-								</div>
-								
-							</div>
-										
-						</article> 	
-						<!-- End article -->	
-					</div> 
-					<!-- end content -->
-				</div> 
-				<!-- end span12 primary -->
+            <?php Exray::load_breadcrumb(); ?>
 
-			</div>
-			<!-- ENd Row -->
-		</div>
-		<!-- End main-cotainer -->
-<?php get_footer();?>
+            <div class="content" role="main">
+
+                <article class="post clearfix" id="post-1" role="article">
+
+                    <header>
+
+                        <h1 class="entry-title" id="search-title"><?php _e('404 Error - Page not Found', 'exray-framework'); ?></h1>
+
+                    </header>
+
+                    <div class="entry-content">	
+
+                        <p><?php _e('Oops! It seems what you are looking are missing, You can search more to find other relevant contents.', 'exray-framework'); ?></p>
+
+                        <div class="search-form-404">
+                            <?php get_search_form(); ?>
+
+                            <?php the_widget('WP_Widget_Categories', 'dropdown=1&count=1'); ?>
+                            <br>
+                            <?php the_widget('WP_Widget_Tag_Cloud'); ?>  
+                        </div>
+
+                    </div>
+
+                </article> 	
+                <!-- End article -->	
+            </div> 
+            <!-- end content -->
+        </div> 
+        <!-- end span12 primary -->
+
+    </div>
+    <!-- ENd Row -->
+</div>
+<!-- End main-cotainer -->
+<?php get_footer(); ?>
